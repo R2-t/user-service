b560fc3a0a4e0bcbf0f105b7dfd90c35e039fe28
Santiago Torres Aragon <contact@santiagotorres.me>
2025-09-17 12:58:03 -0500

    Update models to use UUID

--- models/User.go
+++ models/User.go
@@ -1,24 +1,28 @@
 package models
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
 )
 
 type User struct {
-	gorm.Model
-	Name     string   `json:"name"`
-	Email    string   `gorm:"unique" json:"email"`
-	Password string   `json:"password"`
-	UserTOTP UserTOTP `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	UserID    uuid.UUID  `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	Name      string     `json:"name"`
+	Email     string     `gorm:"unique" json:"email"`
+	Password  string     `json:"password"`
+	CreatedAt *time.Time `gorm:"default:now()"`
+	UpdatedAt *time.Time `gorm:"default:now()"`
+	UserTOTP  UserTOTP   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
 
 type UserTOTP struct {
-	gorm.Model
-	UserId     uint `gorm:"unique"`
+	UserTOTPID uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	UserId     uuid.UUID `gorm:"unique;type:uuid"`
 	Secret     string
 	IsEnabled  bool
 	LastUsedAt *time.Time
+	CreatedAt  *time.Time `gorm:"default:now()"`
 }
 
 type UserSessions struct {
